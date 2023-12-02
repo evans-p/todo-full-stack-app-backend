d75c176ed28e26c3de5f243044629288796a0365
evans-p <evanspoulaki@gmail.com>
2023-12-02 23:21:22 +0200

    Added TodoService for Todo CRUD operations.

---
+++ src/main/java/gr/evansp/todofullstackappbackend/services/todos/TodoService.java
@@ -0,0 +1,41 @@
+package gr.evansp.todofullstackappbackend.services.todos;
+
+import gr.evansp.todofullstackappbackend.models.todos.Todo;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
+/**
+ * Service for CRUD operations regarding Todo1.
+ */
+public interface TodoService {
+  /**
+   * A method to find a Todo1 by its ID (primary key).
+   *
+   * @param id TodoList ID
+   * @return TodoList
+   */
+  Todo find(@NotNull(message = "{id.null}") Long id);
+
+  /**
+   * Stores a new Todo1 to the DB.
+   *
+   * @param todo todo1 to store.
+   * @return newly stored todoList.
+   */
+  Todo store(@NotNull(message = "{todo.list.null}") @Valid Todo todo);
+
+  /**
+   * Updates an existing todo1.
+   *
+   * @param todo todo1 to store.
+   * @return newly stored todoList.
+   */
+  Todo update(@NotNull(message = "{todo.list.null}") @Valid Todo todo);
+
+  /**
+   * Deletes an existing Todo1.
+   *
+   * @param todo todo1 to delete.
+   */
+  void delete(@NotNull(message = "{todo.list.null}") Todo todo);
+}
