2ac72fd3a9a762923aee02aa6b97abce43af8f27
evans-p <evanspoulaki@gmail.com>
2023-12-03 02:41:14 +0200

    Added MessageSource Configuration

---
+++ src/main/java/gr/evansp/todofullstackappbackend/configurations/MessagesConfiguration.java
@@ -0,0 +1,20 @@
+package gr.evansp.todofullstackappbackend.configurations;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+
+@SuppressWarnings("unused")
+@Configuration
+public class MessagesConfiguration {
+
+  @Bean
+  MessageSource messageSource() {
+    ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+
+    messageSource.setBasename("classpath:messages");
+    messageSource.setDefaultEncoding("UTF-8");
+    return messageSource;
+  }
+}
