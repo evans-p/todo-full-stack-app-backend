c94adea70d4c9dabee3ecb54d684c8b0c507cd15
evans-p <evanspoulaki@gmail.com>
2023-12-03 02:42:12 +0200

    Updated GlobalExceptionHandler to handle ConstraintViolationException.

--- src/main/java/gr/evansp/todofullstackappbackend/configurations/GlobalExceptionHandler.java
+++ src/main/java/gr/evansp/todofullstackappbackend/configurations/GlobalExceptionHandler.java
@@ -2,20 +2,26 @@ package gr.evansp.todofullstackappbackend.configurations;
 
 import gr.evansp.todofullstackappbackend.exceptions.NotFoundException;
 import gr.evansp.todofullstackappbackend.utils.messages.ExceptionMessage;
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Locale;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * Application global exception handler.
  */
 @SuppressWarnings("unused")
-@RestControllerAdvice
+@ControllerAdvice
 public class GlobalExceptionHandler {
 
   private final MessageSource messageSource;
@@ -34,5 +40,16 @@ public class GlobalExceptionHandler {
   public ResponseEntity<ExceptionMessage> handleNotFound(NotFoundException e, Locale locale) {
     String errorMessage = messageSource.getMessage(e.getMessage(), e.getArgs(), locale);
     return new ResponseEntity<>(new ExceptionMessage(errorMessage), HttpStatus.NOT_FOUND);
+  }
+
+  @ExceptionHandler(ConstraintViolationException.class)
+  public ResponseEntity<ExceptionMessage> handleInvalid(ConstraintViolationException e, Locale locale) {
+    Map<String, String> messages = e.getConstraintViolations()
+        .stream()
+        .collect(Collectors.toMap(c -> {
+          List<String> paths = Arrays.asList(c.getPropertyPath().toString().split("\\."));
+          return paths.get(paths.size() - 1);
+        }, ConstraintViolation::getMessage));
+    return new ResponseEntity<>(new ExceptionMessage(messages), HttpStatus.NOT_FOUND);
   }
 }
