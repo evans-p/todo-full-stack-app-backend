23c4985c7c498447ca8801c5134deb07296ebae7
evans-p <evanspoulaki@gmail.com>
2024-01-19 22:49:21 +0200

    Updated TodoListController to use TodoListServiceImpl.getAll

--- src/main/java/gr/evansp/todofullstackappbackend/services/todos/TodoListServiceImpl.java
+++ src/main/java/gr/evansp/todofullstackappbackend/services/todos/TodoListServiceImpl.java
@@ -1,12 +1,19 @@
 package gr.evansp.todofullstackappbackend.services.todos;
 
+import gr.evansp.todofullstackappbackend.exceptions.LogicException;
 import gr.evansp.todofullstackappbackend.exceptions.NotFoundException;
+import gr.evansp.todofullstackappbackend.exceptions.UnauthorizedException;
 import gr.evansp.todofullstackappbackend.models.todos.TodoList;
 import gr.evansp.todofullstackappbackend.repositories.todos.TodoListRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
 import org.springframework.validation.annotation.Validated;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 /**
  * Implementation of {@link TodoListService}.
@@ -25,6 +32,17 @@ public class TodoListServiceImpl implements TodoListService {
 
   @Override
   @Transactional
+  public List<TodoList> getAll() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if (authentication == null) {
+      throw new UnauthorizedException(UnauthorizedException.UNAUTHORIZED, null);
+    }
+
+    return todoListRepository.findByUserId(Long.parseLong(authentication.getName()));
+  }
+
+  @Override
+  @Transactional
   public TodoList find(Long id) {
     return todoListRepository.findById(id).orElseThrow(
         () -> new NotFoundException(NotFoundException.LIST_NOT_FOUND, null));
@@ -33,14 +51,23 @@ public class TodoListServiceImpl implements TodoListService {
   @Override
   @Transactional
   public TodoList store(TodoList todoList) {
-    //TODO: add check to make sure to todoList == null
+    if (todoList.getTodoListId() != null) {
+      throw new LogicException(LogicException.ALREADY_EXISTS, null);
+    }
+    checkOwnership(todoList.getUserId());
     return todoListRepository.save(todoList);
   }
 
   @Override
   @Transactional
   public TodoList update(TodoList todoList) {
-    //TODO: add check to make sure to todoList != null
+
+    if (todoList.getTodoListId() == null) {
+      throw new LogicException(LogicException.DOES_NOT_EXIST, null);
+    }
+    checkOwnership(todoList.getUserId());
+
+    todoList.setLastModified(LocalDateTime.now());
     return todoListRepository.save(todoList);
   }
 
@@ -48,5 +75,16 @@ public class TodoListServiceImpl implements TodoListService {
   @Transactional
   public void delete(TodoList todoList) {
     todoListRepository.delete(todoList);
+  }
+
+  private void checkOwnership(Long id) {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if (authentication == null) {
+      throw new UnauthorizedException(UnauthorizedException.UNAUTHORIZED, null);
+    }
+
+    if (Long.parseLong(authentication.getName()) != id) {
+      throw new UnauthorizedException(UnauthorizedException.DIFFERENT_OWNER, null);
+    }
   }
 }
