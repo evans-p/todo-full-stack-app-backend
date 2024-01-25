e1505829809c00085b07ab440854e983f7ca3f0d
evans-p <evanspoulaki@gmail.com>
2024-01-25 23:49:35 +0200

    added new method to TodoRepository, findByUserId.

--- src/main/java/gr/evansp/todofullstackappbackend/repositories/todos/TodoRepository.java
+++ src/main/java/gr/evansp/todofullstackappbackend/repositories/todos/TodoRepository.java
@@ -4,10 +4,19 @@ import gr.evansp.todofullstackappbackend.models.todos.Todo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 /**
  * Repository for Todo1.
  */
+@SuppressWarnings("unused")
 @Repository
 public interface TodoRepository extends JpaRepository<Todo, Long> {
-  //EMPTY
+  /**
+   * Finds all Todo1 of the currently validated user.
+   *
+   * @param userId userId
+   * @return List of Todos
+   */
+  List<Todo> findByUserId(String userId);
 }
